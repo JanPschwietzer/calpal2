fb39ba7d3ce8f8ffff9eed268e5fde1597af0090
Jan Phillip Schwietzer <schwietzer.jan@gmail.com>
2023-11-08 14:41:39 +0100

    formatting

--- lib/helper_widgets/snackbar.dart
+++ lib/helper_widgets/snackbar.dart
@@ -1,10 +1,10 @@
   import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        duration: const Duration(seconds: 2),
-      ),
-    );
-  }
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 2),
+    ),
+  );
+}
